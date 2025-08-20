eb7071b06f24f7505aed11bd142d538480720c7b
kiwi-29 <mohara.oliveira@escola.pr.gov.br>
2025-08-20 07:44:03 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+let numeroAleatorio = Math.floor(Math.random() *100) + 1;
+const palpites = document.querySelector('.palpites');
+const altimoResultado = document.querySelector('.ultimoResultado');
+const baixoOuAlto = document.querySelector('baixoOuAlto');
+const envioPalpite = document.querySelector('.envioPalpite');
+const campoPalpite = document.querySelector('.campoPalpite');
+let contagemPalpites = 1;
+let botaoReiniciar;
 
+ function verificarPalpite() {
+   const palpiteUsuario = Number(campoPalpite.value);
+   if (contagemPalpites === 1) {
+     palpites.textContent = "Palpites anteriores: ";
+}
+
+palpites.textContent += palpiteUsuario + " ";
